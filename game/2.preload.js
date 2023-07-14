e2122a9d92b3dd80d22f257fd9e6cf42587f6ba6
genesis-0000 <thaibao@autonomous.nyc>
2023-07-14 13:46:53 +0700

    - update

--- game/2.preload.js
+++ game/2.preload.js
@@ -9,6 +9,19 @@ function preload() {
     // provider = new ethers.providers.Web3Provider(window.ethereum);
     const rpcUrl = "https://l2-node.regtest.trustless.computer/"; // Replace with your RPC URL
     provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+    //TODO: preload assets
+    if (Object.keys(ASSETS).length > 0) {
+        for (const key in ASSETS) {
+            const value = ASSETS[key]
+            if (value.indexOf("bfs://") > -1) {
+                console.log("asset bfs", key, value);
+            } else {
+                console.log("asset", key, value);
+            }
+
+        }
+    }
 }
 
 
