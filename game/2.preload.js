22c8e6df90e467692af9e5e8099871cf8932fb65
genesis-0000 <thaibao@autonomous.nyc>
2023-07-14 14:48:49 +0700

    - update

--- game/2.preload.js
+++ game/2.preload.js
@@ -1,7 +1,7 @@
 // DO NOT EDIT
 let provider;
 
-function preload() {
+async function preload() {
     if (!window.ethereum) {
         alert("Please install metamask");
         return;
@@ -15,7 +15,22 @@ function preload() {
         for (const key in ASSETS) {
             const value = ASSETS[key]
             if (value.indexOf("bfs://") > -1) {
+                // bfs://chainid/address/file_name
                 console.log("asset bfs", key, value);
+                // split bfs path;
+                const bfsPathArray = value.split("/");
+                const _ = bfsPathArray[2];// chain ID
+                const address = bfsPathArray[3];// address
+                const fileName = bfsPathArray[4];// file_name
+                let contract = new ethers.Contract(BFS_CONTRACTT_ADDRESS, BFS_CONTRACTT_ABI_INTERFACE_JSON, provider);
+                let dataBytesArray = new Uint8Array();
+                for (let i = 0; i < count.toNumber() + 1; i++) {
+                    const chunk = await contract.load(address, fileName, i);
+                    const data = ethers.utils.arrayify(chunk[0]);
+                    dataBytesArray = concatTypedArrays(dataBytesArray, data);
+                }
+                const dataString = toString(dataBytesArray);
+                console.log(dataString);
             } else {
                 console.log("asset", key, value);
             }
