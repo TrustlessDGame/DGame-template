b2a818494db5d9b8669d630cff29c8a68bced092
genesis-0000 <thaibao@autonomous.nyc>
2023-07-17 16:55:06 +0700

    - update

--- game/0.preload_contract_address.js
+++ game/0.preload_contract_address.js
@@ -7,6 +7,5 @@ const GAME_ID = 1;
 const SALT_PASS = "1234";
 
 const GAME_ASSETS = {
-    "asset_1": "bfs://42070/0xE55EAdE1B17BbA28A80a71633aF8C15Dc2D556A5/testimage.jpg",
-    // "asset_1": "./assets/1",
+    "asset_1": "./assets/1",
 }
