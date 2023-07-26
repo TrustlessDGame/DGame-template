a1558e49ec679c7cceb6112e686e0d5c6c911be4
black1714 <black@newbitcoincity.com>
2023-07-26 09:37:58 +0700

    remove reload

--- game/0.const.js
+++ game/0.const.js
@@ -496,7 +496,6 @@ class WalletData {
     this.Wallet = this._formatWalletData(walletData);
     loadNoti("success", "Import private key successfully!", 2500);
     this._checkLogin();
-    window.location.reload();
   };
 
   async _generateAccount(password) {
